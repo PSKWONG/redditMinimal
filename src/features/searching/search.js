ac8aa04e44970cbac8a35316433a6505c49a551f
Edward <pskwong@outlook.com>
2024-10-01 14:51:59 +0100

    FetchSearching Applied

--- src/features/searching/search.js
+++ src/features/searching/search.js
@@ -6,34 +6,41 @@ import { useDispatch, useSelector } from "react-redux";
 //------------------------Import Internal Componenet ----------------------
 import style from './search.module.css';
 import { selectSearchingterms, changeSearchingKeyword } from "./searchSlice";
+import { fetchPostbyTerm } from "./searchSlice";
 
 
 
 
-
+////////////////////////////Conmponenet//////////////////////
 export function SearchingComponenet() {
     let searchingTerms = useSelector(selectSearchingterms);
     const dispatch = useDispatch();
+ //------------------------TextBox Handling  ----------------------   
     const clearSearchGTerm = ()=>{
         dispatch(changeSearchingKeyword(''));
     }
     const handleSearhingTerms=(e)=>{
         dispatch(changeSearchingKeyword(e.target.value));
     }
+//------------------------Searching Fetching ----------------------
+    const handleFetchSearching = (event)=>{
+        event.preventDefault();
+        dispatch(fetchPostbyTerm(searchingTerms));
+    }
 
     
 
     return (
-        <form>
+        <form onSubmit={handleFetchSearching}>
             <input
                 type="text"
                 className={style.inputTextBox}
                 value={searchingTerms}
                 onClick={clearSearchGTerm}
                 onChange={handleSearhingTerms}
-            >
-            </input>
-            <button className={style.searchingBtn} />
+            />
+            
+            <input type="submit" className={style.searchingBtn} value="" />
         </form>
     )
 };
--- src/features/searching/searchSlice.js
+++ src/features/searching/searchSlice.js
@@ -1,22 +1,66 @@
 ////////////////////////////Importing//////////////////////
 
 //------------------------Import External Componenet ----------------------
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 //------------------------Import Internal Componenet ----------------------
 
-const searchingSlice = createSlice({
-    name:'searching',
-    initialState:{
-        keyword:"Search relevant post(s) ",
-    },
-    reducers:{
-        changeSearchingKeyword:(state,action)=>{
-            state.keyword = action.payload
-        }
+
+
+
+export const fetchPostbyTerm = createAsyncThunk(
+    'searching/fetchPostbyTerm',
+    async (terms, thunkAPI) => {
+        
+        const fetchURL = `https://www.reddit.com/search.json?q=${terms}`;
+        const response = await fetch(fetchURL);        
+        const json = await response.json();
+        return json.data;
     }
+)
+
+
+
+
+const searchingSlice = createSlice({
+    name: 'searching',
+    initialState: {
+        keyword: "Search relevant post(s) ",
+        searchResult: {},
+        isLoading: false,
+        hasError: false,
+    },
+    reducers: {
+        changeSearchingKeyword: (state, action) => {
+            state.keyword = action.payload
+        },
+        
+    },
+    
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPostbyTerm.pending, (state) => {
+                state.isLoading = true;
+                state.hasError = false;
+            })
+            .addCase(fetchPostbyTerm.rejected, (state) => {
+                state.isLoading = false;
+                state.hasError = true;
+
+            })
+            .addCase(fetchPostbyTerm.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.hasError = false;
+                const { children } = action.payload
+                state.searchResult = children
+            })
+    },
+    
 });
 
-export const selectSearchingterms = (state)=>state.searching.keyword;
-export const {changeSearchingKeyword} = searchingSlice.actions;
+export const selectSearchingterms = (state) => state.searching.keyword;
+export const selectSearchingLoading = (state) => state.searching.isLoading;
+export const selectSearchingError = (state) => state.searching.hasError;
+
+export const { changeSearchingKeyword } = searchingSlice.actions;
 export default searchingSlice.reducer;
 
